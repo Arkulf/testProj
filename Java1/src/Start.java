2ef62a9835d17b92bb37640a3f0bf0f5717acbb9
epkuad <epkuad@SELIIWDPGSMT405.RND.ERICSSON.SE>
2018-08-16 10:51:47 +0200

    Class  simple added

--- Java1/src/Start.java
+++ Java1/src/Start.java
@@ -4,10 +4,25 @@ public class Start {
 	public static void main(String[] args) {
 		System.out.println("Hello");
         System.out.println(show1());
+        Simple s1 = new Simple();
+        System.out.println(s1.a+ " " +s1.f);
+       
 	}
 
 	private static int show1(){
 		return 1;
+	}
 		
 	}
-}
+
+class Simple{  
+	 int a=10;  
+	 float f=a;
+      Simple(){       
+		}
+	    
+	     
+		
+	
+	}  
+
