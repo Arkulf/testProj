da13200eaf2f917b80db65e85206a735a6bb86d9
epkuad <epkuad@SELIIWDPGSMT405.RND.ERICSSON.SE>
2018-08-16 10:05:48 +0200

    new method show1

--- Java1/src/Start.java
+++ Java1/src/Start.java
@@ -3,7 +3,11 @@ public class Start {
 
 	public static void main(String[] args) {
 		System.out.println("Hello");
-
+        System.out.println(show1());
 	}
 
+	private static int show1(){
+		return 1;
+		
+	}
 }
