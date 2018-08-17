586f932f496e62357ab0d30d1287ecfc10e03bb4
epkuad <epkuad@SELIIWDPGSMT405.RND.ERICSSON.SE>
2018-08-17 13:33:00 +0200

    new instance created

--- Java1/src/Start.java
+++ Java1/src/Start.java
@@ -6,6 +6,7 @@ public class Start {
         System.out.println(show1());
         Simple s1 = new Simple();
         Simple s2 = new Simple(3);
+        Simple s3 = new Simple(99999);
         System.out.println(s1.a+ " " +s1.f);
         System.out.println(s2.a+ " " +s2.f);
        
