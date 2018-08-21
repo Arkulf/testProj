89459a364bd48033d626a523e8227cd78043d82f
epkuad <epkuad@SELIIWDPGSMT410.RND.ERICSSON.SE>
2018-08-21 15:03:13 +0200

    zoo

--- StartUp/src/Animal.java
+++ StartUp/src/Animal.java
@@ -10,4 +10,22 @@ Animal(String typ, String namn, int vikt){
 	System.out.println("A "+ typ + " called " + namn + " is created its weight is " + vikt+ "kg");
 	
 }
+public String getTyp() {
+	return typ;
+}
+public void setTyp(String typ) {
+	this.typ = typ;
+}
+public String getNamn() {
+	return namn;
+}
+public void setNamn(String namn) {
+	this.namn = namn;
+}
+public int getVikt() {
+	return vikt;
+}
+public void setVikt(int vikt) {
+	this.vikt = vikt;
+}
 }
