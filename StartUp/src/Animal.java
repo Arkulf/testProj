cc16ed7cc402d66948bef7afabfc1e813075ed25
epkuad <epkuad@ericsson.com>
2018-08-23 15:37:27 +0200

    adding a forloop

--- StartUp/src/Animal.java
+++ StartUp/src/Animal.java
@@ -1,31 +1,44 @@
 
 public class Animal {
-	String typ,namn;
+	String typ, namn;
 	int vikt;
-Animal(String typ, String namn, int vikt){
-	this.typ=typ;
-	this.namn=namn;
-	this.vikt=vikt;
+
+	Animal(String typ, String namn, int vikt) {
+		this.typ = typ;
+		this.namn = namn;
+		this.vikt = vikt;
+
+		System.out.println("A " + typ + " called " + namn + " is created its weight is " + vikt + "kg");
+
+	}
+
+	public String getTyp() {
+		return typ;
+	}
+
+	public void setTyp(String typ) {
+		this.typ = typ;
+	}
+
+	public String getNamn() {
+		return namn;
+	}
+
+	public void setNamn(String namn) {
+		this.namn = namn;
+	}
+
+	public int getVikt() {
+		return vikt;
+	}
+
+	public void setVikt(int vikt) {
+		this.vikt = vikt;
+	}
 	
-	System.out.println("A "+ typ + " called " + namn + " is created its weight is " + vikt+ "kg");
-	
-}
-public String getTyp() {
-	return typ;
-}
-public void setTyp(String typ) {
-	this.typ = typ;
-}
-public String getNamn() {
-	return namn;
-}
-public void setNamn(String namn) {
-	this.namn = namn;
-}
-public int getVikt() {
-	return vikt;
-}
-public void setVikt(int vikt) {
-	this.vikt = vikt;
-}
+	public String toString() {
+        return "Namn: " + this.namn + ",, "
+                + "Typ: " + this.typ + ",, " +  "vikt: " + this.vikt;
+    }
+
 }
--- StartUp/src/Start.java
+++ StartUp/src/Start.java
@@ -1,40 +1,45 @@
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Random; 
+import java.util.Random;
+
 public class Start {
 
-  //       animals = new String[10];
-         static String[] animals = { 
-        		    "Dog", "cat", "Pig",
-        		    "Horse", "Cow", "Elephant", 
-        		    "Crocodile","Bull","Bird","Ant","Mouse"
-        		};
-         static String[] names;
-         static Random rand = new Random();
-         static int x=0;
-         static ArrayList<Animal> zoo = new ArrayList<Animal>();
-         
+	// animals = new String[10];
+	static String[] animals = { "Dog", "cat", "Pig", "Horse", "Cow", "Elephant", "Crocodile", "Bull", "Bird", "Ant",
+			"Mouse" };
+	static String[] names;
+	static Random rand = new Random();
+	static int x = 0;
+	static ArrayList<Animal> zoo = new ArrayList<Animal>();
+
 	public static void main(String[] args) {
-		        
-        ReadToArray rFile = new ReadToArray();
-        try {
-			names=rFile.readLines("namesList.txt");
+
+		ReadToArray rFile = new ReadToArray();
+		try {
+			names = rFile.readLines("namesList.txt");
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
+
 			e.printStackTrace();
-		}     
-        
-        while (x<rand.nextInt(100)){
-        createAnimals(x, rand.nextInt(10),names[rand.nextInt(908)]);
-        x++;}
+		}
+
+		while (x < rand.nextInt(100)) {
+			createAnimals(x, rand.nextInt(10), names[rand.nextInt(908)]);
+			x++;
+		}
+		int a=0;
+	    for (Animal tmpAnimal:zoo){
+	    	a++;
+	    	System.out.println("djur " +a + ": " + tmpAnimal.toString());
+	    }
+	
+	}
+	
+
+	// Method Create animals
+
+	private static void createAnimals(int no, int antype, String name) {
+		zoo.add(new Animal(animals[antype], name, rand.nextInt(1000)));
 	}
 
-		
-	//Method Create animals
-	
-			private static void createAnimals(int no,int antype, String name){				
-			zoo.add(new Animal(animals[antype],name,rand.nextInt(1000)));
-			}			
-	
-}  
-
+}
